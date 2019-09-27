6e9f25cfd26c8f426a3ab5e9b74ea8758c5e67bb
842449801 <842449801@qq.com>
2019-09-27 19:02:33 +0800

    1.opengl 初始化v1

--- app/src/main/java/com/zdw/baseopengl/MainActivity.java
+++ app/src/main/java/com/zdw/baseopengl/MainActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import com.zdw.baseopengl.render.MyGLRenderer;
 import com.zdw.baseopengl.render.Square;
 import com.zdw.baseopengl.render.Triangle;
+import com.zdw.baseopengl.render.Trianglev2;
 
 public class MainActivity extends AppCompatActivity {
 
     GLSurfaceView glSurfaceView;
-    Square square;
-    Triangle triangle;
+//    Square square;
+//    Triangle triangle;
+    Trianglev2 trianglev2;
     boolean rendererSet;
-//    MyGLRenderer mRenderer;
+//    MyGLRenderer myGLRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,29 @@ public class MainActivity extends AppCompatActivity {
 
         // 创建OpenGL ES 2.0的上下文
         glSurfaceView.setEGLContextClientVersion(2);
-//        mRenderer = new MyGLRenderer();
-        Square square = new Square(glSurfaceView);
+//        myGLRenderer = new MyGLRenderer();
+        trianglev2 = new Trianglev2();
         //设置Renderer用于绘图
-        glSurfaceView.setRenderer(square);
+//        glSurfaceView.setRenderer(myGLRenderer);
+        glSurfaceView.setRenderer(trianglev2);
         //只有在绘制数据改变时才绘制view，可以防止GLSurfaceView帧重绘
         glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//        if (glSurfaceView != null)
+//            glSurfaceView.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        if (glSurfaceView != null)
+//            glSurfaceView.onPause();
+    }
 }
--- app/src/main/java/com/zdw/baseopengl/render/MyGLRenderer.java
+++ app/src/main/java/com/zdw/baseopengl/render/MyGLRenderer.java
@@ -2,25 +2,35 @@ package com.zdw.baseopengl.render;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
-    private Triangle mTriangle;
+    private static final String TAG = MyGLRenderer.class.getSimpleName();
+
+//    private Triangle mTriangle;
+    private Trianglev2 mTriangle;
 
     @Override
     public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
-        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);//白色不透明
-        mTriangle = new Triangle();
+        Log.i(TAG, "onSurfaceCreated: ");
+//        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);//白色不透明
+        GLES20.glClearColor(0.5f,0.5f,0.5f,1.0f);
+//        mTriangle = new Triangle();
+        mTriangle = new Trianglev2();
     }
 
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {//当view的几何形状发生变化时调用，比如设备屏幕方向改变时
+        Log.i(TAG, "onSurfaceChanged: ");
         //绘制窗口
         GLES20.glViewport(0, 0, width, height);
     }
 
+    @Override
     public void onDrawFrame(GL10 unused) {//每次重绘view时调用
+        Log.i(TAG, "onDrawFrame: ");
         //重绘背景色
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         mTriangle.draw();
